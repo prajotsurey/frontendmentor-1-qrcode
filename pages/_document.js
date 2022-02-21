252e355c96c3abc5d59df25f8164defb646c9995
prajotsurey <prajotsurey@gmail.com>
2022-02-22 02:46:06 +0530

    fixed accesibility issues

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" type='image/png' href="/images/favicon-32x32.png" sizes='32x32'/>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <title>
