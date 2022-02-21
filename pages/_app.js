2eebe48d8346422f27dd2a2118380273c8354dd6
prajotsurey <prajotsurey@gmail.com>
2022-02-22 02:31:28 +0530

    extracted footer to app.js and added main element to index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../styles/styles.scss'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Card from '../components/Card'
-import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Card />
-      <Footer />
-    </>
+    </main>
   )
 }
