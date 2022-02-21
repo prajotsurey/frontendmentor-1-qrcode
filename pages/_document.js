4099965b03aa19d546dadc96ef7f2af94da48352
prajotsurey <prajotsurey@gmail.com>
2022-02-22 02:37:29 +0530

    fixed accesibility issues

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,16 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='en-IN'>
         <Head>
           <link rel="icon" type='image/png' href="/images/favicon-32x32.png" sizes='32x32'/>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
+        <title>
+          FrontendMentor QrCode Challenge
+        </title>
         <body>
           <Main />
           <NextScript />
